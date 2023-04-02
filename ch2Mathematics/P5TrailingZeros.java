a1d8be34e946969bae04346c2f2cc17491810be0
Anurag Kumar <anuragkr41@gmail.com>
2023-04-02 23:49:51 +0530

    Trailing zeros Corrected

--- ch2Mathematics/P5TrailingZeros.java
+++ ch2Mathematics/P5TrailingZeros.java
@@ -3,12 +3,21 @@ package com.geekForGeeksDSCourse.ch2Mathematics;
 public class P5TrailingZeros {
     public static void main(String[] args) {
 
-        Integer input = 15;
+        Integer input = 10;
         System.out.println(new P5Solver().solve(input));
 
         //I was able to solve this using the O(n) complexity but a better solution is to just
         // return the division of the number by 5 which will take the O(1);
         // I have made the sure that the overflow issues is tackled in the problem
+
+        /*
+        My solution was wrong as I didn't considered the case that what will happen when n = 25 as there will be 2 5s available
+
+
+        Going by the solution, we will check only the number of 5s available and if n is divisible by i
+         */
+
+
 
 
     }
@@ -23,10 +32,8 @@ class P5Solver{
         2X5 then how many there is 10 available
          */
         int count =0;
-        for(int i =1; i <= num; i++){
-            if(i%10==0 || i%5==0){
-                count++;
-            }
+        for(int i =5; i <= num; i=i*5){
+            count = count + num/i;
         }
 
 
