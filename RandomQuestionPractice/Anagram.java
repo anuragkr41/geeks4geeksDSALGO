b6215ce411b370596868aefe10439ebd6c0118f6
Anurag Kumar <anuragkr41@gmail.com>
2023-04-20 14:26:18 +0530

    Interrupttion Anagram

---
+++ RandomQuestionPractice/Anagram.java
@@ -0,0 +1,20 @@
+package com.geekForGeeksDSCourse.RandomQuestionPractice;
+
+public class Anagram {
+    /*
+    We have two strings and we have to check if the strings are anagram or not
+     */
+
+    private static final String str1="abcde";
+    private static final String str2="bcaed";
+    public static void main(String[] args) {
+        System.out.println(new AnaSolver().isAnagram(str1, str2));
+
+    }
+
+}
+class AnaSolver{
+    public static boolean isAnagram(String s1, String s2){
+        return false;
+    }
+}
